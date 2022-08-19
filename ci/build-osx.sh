31e670ffb17b30cef7c2dd3aa5476c927cf270b6
Vladimir <summeroff@gmail.com>
2022-08-19 14:07:23 -0700

    add github actions (#17)

--- ci/build-osx.sh
+++ ci/build-osx.sh
@@ -1,3 +1,4 @@
+set -e
 mkdir build
 cd build
 
@@ -5,12 +6,15 @@ cd build
 cmake .. \
 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.11 \
 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
--DCMAKE_INSTALL_PREFIX=${DISTRIBUTEDIRECTORY}/font-manager
--DNODEJS_NAME=${RUNTIMENAME} \
--DNODEJS_URL=${RUNTIMEURL} \
--DNODEJS_VERSION=${RUNTIMEVERSION}
+-DNODEJS_VERSION=${ELECTRON_VERSION} \
+-DCMAKE_INSTALL_PREFIX=${DISTRIBUTE_DIRECTORY}/font-manager
 
 cd ..
 
 # Build
 cmake --build build --target install --config RelWithDebInfo
+
+#Upload debug files
+curl -sL https://sentry.io/get-cli/ | bash
+dsymutil $PWD/${BUILD_DIRECTORY}/RelWithDebInfo/node_fontmanager.node
+sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif --org streamlabs-desktop --project obs-client $PWD/${BUILD_DIRECTORY}/RelWithDebInfo/node_fontmanager.node.dSYM/Contents/Resources/DWARF/node_fontmanager.node 
