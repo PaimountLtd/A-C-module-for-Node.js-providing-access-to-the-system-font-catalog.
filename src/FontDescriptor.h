73543d83e6fd1d8b8579563b184028dc6b7a8c87
unknown <stevensoftware52@gmail.com>
2022-10-07 17:17:56 -0400

    Clang format

--- src/FontDescriptor.h
+++ src/FontDescriptor.h
@@ -6,142 +6,150 @@
 #include <vector>
 
 enum FontWeight {
-  FontWeightUndefined   = 0,
-  FontWeightThin        = 100,
-  FontWeightUltraLight  = 200,
-  FontWeightLight       = 300,
-  FontWeightNormal      = 400,
-  FontWeightMedium      = 500,
-  FontWeightSemiBold    = 600,
-  FontWeightBold        = 700,
-  FontWeightUltraBold   = 800,
-  FontWeightHeavy       = 900
+	FontWeightUndefined = 0,
+	FontWeightThin = 100,
+	FontWeightUltraLight = 200,
+	FontWeightLight = 300,
+	FontWeightNormal = 400,
+	FontWeightMedium = 500,
+	FontWeightSemiBold = 600,
+	FontWeightBold = 700,
+	FontWeightUltraBold = 800,
+	FontWeightHeavy = 900
 };
 
 enum FontWidth {
-  FontWidthUndefined      = 0,
-  FontWidthUltraCondensed = 1,
-  FontWidthExtraCondensed = 2,
-  FontWidthCondensed      = 3,
-  FontWidthSemiCondensed  = 4,
-  FontWidthNormal         = 5,
-  FontWidthSemiExpanded   = 6,
-  FontWidthExpanded       = 7,
-  FontWidthExtraExpanded  = 8,
-  FontWidthUltraExpanded  = 9
+	FontWidthUndefined = 0,
+	FontWidthUltraCondensed = 1,
+	FontWidthExtraCondensed = 2,
+	FontWidthCondensed = 3,
+	FontWidthSemiCondensed = 4,
+	FontWidthNormal = 5,
+	FontWidthSemiExpanded = 6,
+	FontWidthExpanded = 7,
+	FontWidthExtraExpanded = 8,
+	FontWidthUltraExpanded = 9
 };
 
 struct FontDescriptor {
 public:
-  const char *path;
-  const char *postscriptName;
-  const char *family;
-  const char *style;
-  FontWeight weight;
-  FontWidth width;
-  bool italic;
-  bool oblique;
-  bool monospace;
-  
-  FontDescriptor(Napi::Object obj) {
-    path = NULL;
-    postscriptName = obj.Get("postscriptName").ToString().Utf8Value().c_str();
-    family = obj.Get("family").ToString().Utf8Value().c_str();
-    style = obj.Get("style").ToString().Utf8Value().c_str();
-    weight = (FontWeight) obj.Get("weight").ToNumber().Int64Value();
-    width = (FontWidth) obj.Get("width").ToNumber().Int64Value();
-    italic = obj.Get("italic").ToBoolean().Value();
-    oblique = obj.Get("oblique").ToBoolean().Value();
-    monospace = obj.Get("monospace").ToBoolean().Value();
-  }
+	const char *path;
+	const char *postscriptName;
+	const char *family;
+	const char *style;
+	FontWeight weight;
+	FontWidth width;
+	bool italic;
+	bool oblique;
+	bool monospace;
 
-  FontDescriptor() {
-    path = NULL;
-    postscriptName = NULL;
-    family = NULL;
-    style = NULL;
-    weight = FontWeightUndefined;
-    width = FontWidthUndefined;
-    italic = false;
-    oblique = false;
-    monospace = false;
-  }
-  
-  FontDescriptor(const char *path, const char *postscriptName, const char *family, const char *style, 
-                 FontWeight weight, FontWidth width, bool italic, bool oblique, bool monospace) {
-    this->path = copyString(path);
-    this->postscriptName = copyString(postscriptName);
-    this->family = copyString(family);
-    this->style = copyString(style);
-    this->weight = weight;
-    this->width = width;
-    this->italic = italic;
-    this->oblique = oblique;
-    this->monospace = monospace;
-  }
+	FontDescriptor(Napi::Object obj)
+	{
+		path = NULL;
+		postscriptName = obj.Get("postscriptName").ToString().Utf8Value().c_str();
+		family = obj.Get("family").ToString().Utf8Value().c_str();
+		style = obj.Get("style").ToString().Utf8Value().c_str();
+		weight = (FontWeight)obj.Get("weight").ToNumber().Int64Value();
+		width = (FontWidth)obj.Get("width").ToNumber().Int64Value();
+		italic = obj.Get("italic").ToBoolean().Value();
+		oblique = obj.Get("oblique").ToBoolean().Value();
+		monospace = obj.Get("monospace").ToBoolean().Value();
+	}
 
-  FontDescriptor(FontDescriptor *desc) {
-    path = copyString(desc->path);
-    postscriptName = copyString(desc->postscriptName);
-    family = copyString(desc->family);
-    style = copyString(desc->style);
-    weight = desc->weight;
-    width = desc->width;
-    italic = desc->italic;
-    oblique = desc->oblique;
-    monospace = desc->monospace;
-  }
-  
-  ~FontDescriptor() {
-    if (path)
-      delete path;
-    
-    if (postscriptName)
-      delete postscriptName;
-    
-    if (family)
-      delete family;
-    
-    if (style)
-      delete style;
-    
-    postscriptName = NULL;
-    family = NULL;
-    style = NULL;
-  }
-  
-  Napi::Object toJSObject(napi_env env) {
-    Napi::Object res = Napi::Object::New(env);
-    res.Set("path", Napi::String::New(env, path));
-    res.Set("postscriptName", Napi::String::New(env, postscriptName));
-    res.Set("family", Napi::String::New(env, family));
-    res.Set("style", Napi::String::New(env, style));
-    res.Set("weight", Napi::Number::New(env, weight));
-    res.Set("width", Napi::Number::New(env, width));
-    res.Set("italic", Napi::Boolean::New(env, italic));
-    res.Set("oblique", Napi::Boolean::New(env, oblique));
-    res.Set("monospace", Napi::Boolean::New(env, monospace));
-    return res;
-  }
-  
+	FontDescriptor()
+	{
+		path = NULL;
+		postscriptName = NULL;
+		family = NULL;
+		style = NULL;
+		weight = FontWeightUndefined;
+		width = FontWidthUndefined;
+		italic = false;
+		oblique = false;
+		monospace = false;
+	}
+
+	FontDescriptor(const char *path, const char *postscriptName, const char *family, const char *style, FontWeight weight, FontWidth width, bool italic, bool oblique,
+		       bool monospace)
+	{
+		this->path = copyString(path);
+		this->postscriptName = copyString(postscriptName);
+		this->family = copyString(family);
+		this->style = copyString(style);
+		this->weight = weight;
+		this->width = width;
+		this->italic = italic;
+		this->oblique = oblique;
+		this->monospace = monospace;
+	}
+
+	FontDescriptor(FontDescriptor *desc)
+	{
+		path = copyString(desc->path);
+		postscriptName = copyString(desc->postscriptName);
+		family = copyString(desc->family);
+		style = copyString(desc->style);
+		weight = desc->weight;
+		width = desc->width;
+		italic = desc->italic;
+		oblique = desc->oblique;
+		monospace = desc->monospace;
+	}
+
+	~FontDescriptor()
+	{
+		if (path)
+			delete path;
+
+		if (postscriptName)
+			delete postscriptName;
+
+		if (family)
+			delete family;
+
+		if (style)
+			delete style;
+
+		postscriptName = NULL;
+		family = NULL;
+		style = NULL;
+	}
+
+	Napi::Object toJSObject(napi_env env)
+	{
+		Napi::Object res = Napi::Object::New(env);
+		res.Set("path", Napi::String::New(env, path));
+		res.Set("postscriptName", Napi::String::New(env, postscriptName));
+		res.Set("family", Napi::String::New(env, family));
+		res.Set("style", Napi::String::New(env, style));
+		res.Set("weight", Napi::Number::New(env, weight));
+		res.Set("width", Napi::Number::New(env, width));
+		res.Set("italic", Napi::Boolean::New(env, italic));
+		res.Set("oblique", Napi::Boolean::New(env, oblique));
+		res.Set("monospace", Napi::Boolean::New(env, monospace));
+		return res;
+	}
+
 private:
-  char *copyString(const char *input) {
-    if (!input)
-      return NULL;
+	char *copyString(const char *input)
+	{
+		if (!input)
+			return NULL;
 
-    char *str = new char[strlen(input) + 1];
-    strcpy(str, input);
-    return str;
-  }
+		char *str = new char[strlen(input) + 1];
+		strcpy(str, input);
+		return str;
+	}
 };
 
 class ResultSet : public std::vector<FontDescriptor *> {
 public:
-  ~ResultSet() {
-    for (ResultSet::iterator it = this->begin(); it != this->end(); it++) {
-      delete *it;
-    }
-  }
+	~ResultSet()
+	{
+		for (ResultSet::iterator it = this->begin(); it != this->end(); it++) {
+			delete *it;
+		}
+	}
 };
 
 #endif
